5f1c854881263152fcbba49f2c20bce64e5ab78b
olahmark <103537814+olahmark@users.noreply.github.com>
2022-05-29 10:20:22 +0200

    Revert "added dropdown menu"
    This reverts commit 9266dd621b1c16b4f9c9aefefd964b5148bf9157.

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,7 +4,6 @@ import * as ReactDOM from "react-dom/client"
 import { App } from "./App"
 import reportWebVitals from "./reportWebVitals"
 import * as serviceWorker from "./serviceWorker"
-//import "./components/menu/dropdown.css"
 
 const container = document.getElementById("root")
 const root = ReactDOM.createRoot(container)
