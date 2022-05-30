9d79bc70377a6788a5b4f78fea60f70d1259e8fd
pethotibor <petho.tibor@gmail.com>
2022-05-30 22:37:53 +0200

    added UtilConfig

---
+++ backend/src/main/java/hu/nye/webapp/cardealership/configuration/UtilConfiguration.java
@@ -0,0 +1,14 @@
+package hu.nye.webapp.cardealership.configuration;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class UtilConfiguration {
+    
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
